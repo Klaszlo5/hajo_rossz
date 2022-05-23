a09116889cd2116b50ccdd5c62b9119766f0fc77
gabor19 <sardigabor09@gmail.com>
2022-05-23 13:27:12 +0200

    first commit

--- src/torpedoprojekt/Hajo.java
+++ src/torpedoprojekt/Hajo.java
@@ -4,6 +4,19 @@ public class Hajo {
 
 
     public static void main(String[] args) {
+        private int[] poz;
+    
+    Hajo(int[] poz){
+        this.poz=poz;
+    }
+    
+    public String talalt(int poz){
+        int i = 0, N=this.poz.length;
+        while (i<N && !(poz[i]==poz)) {
+        i++;
+        }
+        return i < N ? "nem talált";
+    }
     }
 
 }
