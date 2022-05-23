c30dbd298725e8986044a984843765c41a8172ff
gabor19 <sardigabor09@gmail.com>
2022-05-23 13:21:40 +0200

    first commit

---
+++ src/torpedoprojekt/Hajo.java
@@ -0,0 +1,9 @@
+package torpedoprojekt;
+
+public class Hajo {
+
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/torpedoprojekt/TorpedoProjekt.java
@@ -0,0 +1,18 @@
+package torpedoprojekt;
+
+public class TorpedoProjekt {
+
+
+    public static void main(String[] args) {
+        System.out.println("Teszt");
+        assert 3==5;
+    }
+    public String tesztLoves(int poz){
+        Hajo hajo 
+                
+        return "";
+    }
+    public static void main(int szam) {
+        
+    }
+}
